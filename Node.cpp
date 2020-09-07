9f4705a71c6f9bf55dc9f1979b4a08b06902b4c6
Charles-E-Williams <70922343+Charles-E-Williams@users.noreply.github.com>
2020-09-07 18:28:50 -0500

    Add files via upload

---
+++ Node.cpp
@@ -0,0 +1,14 @@
+#include "Node.h"
+using namespace std;
+
+// Default constructor
+Node::Node(): data(),next(nullptr) {} // remember to initialize next to nullptr
+
+// Parameterized constructor
+Node::Node(string id, int year, int month, double temperature)
+: data(id,year,month,temperature), next(nullptr) {}
+// remember to initialize next to nullptr
+
+bool Node::operator<(const Node& b) {
+	return (this->data < b.data); 
+}
